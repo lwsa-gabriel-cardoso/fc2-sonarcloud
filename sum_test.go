d25da27492666516d443d9188f949ad362f91f9b
Gabriel <t.gabriel.cardoso@lwsa.tech>
2025-10-30 08:38:26 -0300

    feat: Add new tests

--- sum_test.go
+++ sum_test.go
@@ -1,10 +1,22 @@
 package main
 
 import "testing"
-func TestSum(t  *testing.T) {
+
+func TestSum(t *testing.T) {
 	result := sum(2, 2)
-	
+
 	if result != 4 {
 		t.Errorf("Sum(2, 2) = %d; want 4", result)
 	}
+
+	result = sub(2, 2)
+	if result != 0 {
+		t.Errorf("Sub(2, 2) = %d; want 0", result)
+	}
+
+	result = times(2, 2)
+	if result != 4 {
+		t.Errorf("Times(2, 2) = %d; want 4", result)
+	}
+
 }
